14c964517b43f921909d5f118f3469333326d017
Anthony Moore <spitfyre03@gmail.com>
2022-02-16 04:42:18 -0500

    Added basic react-for-role feature
    Implemented a basic map in the format {guild -> {message -> role, ...}, ...}, where a map of guilds is associated with maps of messages to roles. For example, guild ServerA will be associated to a map where each entry in that map is a message that, when reacted to, results in the reacting user being granted the role associated to that message.
    An example is currently hardcoded for personal use, but a more verbose and customizable system will be implemented in the future

--- src/main/java/com/github/waifu/App.java
+++ src/main/java/com/github/waifu/App.java
@@ -4,11 +4,15 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.*;
+import net.dv8tion.jda.api.events.message.guild.react.GuildMessageReactionAddEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Spitfyre03
@@ -16,6 +20,9 @@ import java.io.IOException;
 public class App extends ListenerAdapter {
 
 	public static String TOKEN;
+
+	// Guild, Message, Role
+	public static final Map<Long, Map<Long, Long>> reactionMap = new HashMap<>();
 
 	public static void main(String[] args) throws InterruptedException {
 		try {
@@ -46,6 +53,27 @@ public class App extends ListenerAdapter {
 			return;
 		}
 
+		Map<Long, Long> msgRoleMap = new HashMap<>();
+		msgRoleMap.put(942656923390590996L, 942646740845232148L);
+		reactionMap.put(879891493840617543L, msgRoleMap);
+
 		bot.awaitReady();
+	}
+
+	@Override
+	public void onGuildMessageReactionAdd(@Nonnull GuildMessageReactionAddEvent event) {
+		Map<Long, Long> msgRoleMap;
+		Guild server = event.getGuild();
+		msgRoleMap = reactionMap.get(server.getIdLong());
+		if (msgRoleMap != null) {
+			Long roleLong = msgRoleMap.get(event.getMessageIdLong());
+			if (roleLong != null) {
+				Role role = server.getRoleById(roleLong);
+				if (role != null) {
+					event.retrieveMember().queue(member -> server.addRoleToMember(member, role).queue());
+				}
+			}
+		}
+		// else no react-for-role maps set up for this server
 	}
 }
