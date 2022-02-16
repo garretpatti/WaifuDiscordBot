e533ab2d240930e19e503471df6d0d1596b3487f
Anthony Moore <spitfyre03@gmail.com>
2022-02-16 04:55:39 -0500

    NSFW - Added nh quick command
     + Added !nh command that accepts a 6 digit number and validates if the extension is valid before reposting it to the target channel. More features planned in the future

--- src/main/java/com/github/waifu/App.java
+++ src/main/java/com/github/waifu/App.java
@@ -15,9 +15,13 @@ import org.json.JSONObject;
 import javax.annotation.Nonnull;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Consumer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author Spitfyre03
@@ -110,6 +114,41 @@ public class App extends ListenerAdapter {
 				}
 				else if (strMsg.startsWith("!tenor") && strMsg.length() > 7) {
 					TenorHandler.getSearchResults(strMsg.substring(7), memeResponseConsumer, memeErrorConsumer);
+				}
+				else if (textChannel.isNSFW() && strMsg.startsWith("!nh") && strMsg.length() >= 10){
+					Matcher matcher = Pattern.compile("\\d{6}").matcher(strMsg.substring(4, 10));
+					if (matcher.find()) {
+						String ext = matcher.group(0);
+						new Thread(() -> {
+							String url = String.format("https://nhentai.net/g/%1$s", ext);
+							HttpURLConnection connection = null;
+							try {
+								connection = (HttpURLConnection) new URL(url).openConnection();
+								// Get request
+								connection.setDoInput(true);
+								connection.setDoOutput(true);
+								connection.setRequestMethod("GET");
+								connection.setRequestProperty("Content-Type", "text/html");
+								connection.setRequestProperty("Accept", "text/html");
+								connection.setRequestProperty("Content-Type", "text/html; charset=UTF-8");
+
+								// Handle failure
+								int statusCode = connection.getResponseCode();
+								if (statusCode != HttpURLConnection.HTTP_OK && statusCode != HttpURLConnection.HTTP_CREATED) {
+									textChannel.sendMessage("Sorry Senpai, I couldn't find that one.").queue();
+								}
+								else {
+									textChannel.sendMessage(url).queue();
+								}
+							}
+							catch (Exception ignored) {}
+							finally {
+								if (connection != null) {
+									connection.disconnect();
+								}
+							}
+						}).start();
+					}
 				}
 			}
 		}
