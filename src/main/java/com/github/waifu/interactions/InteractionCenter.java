c772e19a24dc54171271a03063d126c9d32a5eec
Anthony Moore <spitfyre03@gmail.com>
2022-05-06 13:49:12 -0400

    Added automatic deletion of unregistered commands

--- src/main/java/com/github/waifu/interactions/InteractionCenter.java
+++ src/main/java/com/github/waifu/interactions/InteractionCenter.java
@@ -7,15 +7,15 @@ import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.Command;
 import net.dv8tion.jda.internal.utils.Checks;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nonnull;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
 public class InteractionCenter extends ListenerAdapter {
 
@@ -31,61 +31,97 @@ public class InteractionCenter extends ListenerAdapter {
 
     // should be called after bot starts up
     public void registerCommands(@Nonnull JDA bot) {
-        LOGGER.info("Registering slash commands for WDO");
+        LOGGER.info("Registering slash commands for Waifu");
         // Add a new instance of your command handler here to register it
-        List.of(
-                new SlashNh(),
-                new SlashBasicResponse("ping", "Pong!", "Ping test"),
-                new SlashBasicResponse("bing", "Bong!", "Bing bong!"),
-                new SlashBaseTenorSearch("smashing", "Smashing!", "nigel thornberry smashing"),
-                new SlashBaseTenorSearch("cagemebro", "I'm going to steal the Declaration of Independence", "nick cage"),
-                new SlashBaseTenorSearch("deuces", "Peace bitches", "deuces"),
-                new SlashPokemon(),
-                new SlashBasicResponse("kanyon", "bruh", "Bing bong!"),
-                new SlashMagic8(),
-                new SlashEmote()
-        ).forEach(t -> {
-            // TODO Discord uses the crazy regex as the naming rule for commands
-            // ^[-_\p{L}\p{N}\p{sc=Deva}\p{sc=Thai}]{1,32}$
-            try {
-                Checks.notBlank(t.getName(),"Command name");
-            }
-            catch (IllegalStateException e) {
-                LOGGER.error(
-                    String.format(
-                        "A command of Type %s with no name was provided. It will not be registered.",
-                        t.getClass().getSimpleName()),
-                    e);
-                return;
-            }
+        List<ISlashInteraction> commandsToRegister = List.of(
+            new SlashNh(),
+            new SlashBasicResponse("ping", "Pong!", "Ping test"),
+            new SlashBasicResponse("bing", "Bong!", "Bing bong!"),
+            new SlashBaseTenorSearch("smashing", "Smashing!", "nigel thornberry smashing"),
+            new SlashBaseTenorSearch("cagemebro", "I'm going to steal the Declaration of Independence", "nick cage"),
+            new SlashBaseTenorSearch("deuces", "Peace bitches", "deuces"),
+            new SlashPokemon(),
+            new SlashMagic8(),
+            new SlashEmote(),
+            new SlashBasicResponse("oauth2",
+                "https://discord.com/api/oauth2/authorize?client_id=933960413534617611&permissions=1574075624529&scope=bot%20applications.commands",
+                "Get the invite link for this bot")
+        );
 
-            List<Long> guilds = t.getGuilds();
-            if (Optional.ofNullable(guilds).orElse(List.of()).isEmpty()) {
-                bot.upsertCommand(t.getCommand()).queue(command -> {
-                    LOGGER.info(String.format("Global command %s registered", command.getName()));
-                });
-            }
-            else {
-                guilds.forEach( g -> {
-                    Guild guild = bot.getGuildById(g);
-                    if (guild != null) {
-                        guild.upsertCommand(t.getCommand()).queue(command -> {
-                            LOGGER.info(String.format("Command %s registered for guild %d", command.getName(), g));
+        CompletableFuture<List<Command>> globalCommandsFuture = bot.retrieveCommands().submit();
+        Map<Long, CompletableFuture<List<Command>>> guildCommandsFuture = new HashMap<>();
+        bot.getGuilds().forEach(g -> {
+            guildCommandsFuture.put(g.getIdLong(), g.retrieveCommands().submit());
+        });
+        CompletableFuture.allOf(guildCommandsFuture.values().toArray(new CompletableFuture[0])).thenAcceptBothAsync(
+            globalCommandsFuture, (_void, globalCommands) -> {
+                Map<Long, List<Command>> guildCommands = guildCommandsFuture
+                    .entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, v -> v.getValue().join()));
+                List<CompletableFuture<Command>> globalQueued = new ArrayList<>();
+                List<CompletableFuture<Command>> guildQueued = new ArrayList<>();
+                commandsToRegister.forEach(t -> {
+                    // TODO Discord uses the crazy regex as the naming rule for commands
+                    // ^[-_\p{L}\p{N}\p{sc=Deva}\p{sc=Thai}]{1,32}$
+                    try {
+                        Checks.notBlank(t.getName(), "Command name");
+                    }
+                    catch (IllegalStateException e) {
+                        LOGGER.error(
+                            String.format(
+                                "A command of Type %s with no name was provided. It will not be registered.",
+                                t.getClass().getSimpleName())
+                            , e
+                        );
+                        return;
+                    }
+
+                    List<Long> guildsToRegister = t.getGuilds();
+                    if (Optional.ofNullable(guildsToRegister).orElse(List.of()).isEmpty()) {
+                        globalQueued.add(bot.upsertCommand(t.getCommand()).submit());
+                        LOGGER.debug(String.format("Global command %s queued to register", t.getName()));
+                        globalCommands.removeIf(gCmd -> gCmd.getName().equals(t.getName()));
+                    }
+                    else {
+                        guildsToRegister.forEach(g -> {
+                            Guild guildToRegister = bot.getGuildById(g);
+                            if (guildToRegister != null) {
+                                guildQueued.add(guildToRegister.upsertCommand(t.getCommand()).submit());
+                                LOGGER.debug(String.format("Command %s queued to register for guild %d", t.getName(), g));
+                                guildCommands.get(g).removeIf(gCmd -> gCmd.getName().equals(t.getName()));
+                            }
+                        });
+                    }
+                    slashHandlers.put(t.getName(), t);
+
+                    if (t instanceof IButtonInteraction handler) {
+                        handler.getButtons().forEach(b -> {
+                            LOGGER.debug(String.format("Registering button handler %s for command %s",
+                                b.getId(),
+                                t.getName()));
+                            buttonHandlers.put(b.getId(), handler);
                         });
                     }
                 });
-            }
-            slashHandlers.put(t.getName(), t);
-
-            if (t instanceof IButtonInteraction handler) {
-                handler.getButtons().forEach(b -> {
-                    LOGGER.info(String.format("Registering button handler %s for command %s",
-                            b.getId(),
-                            t.getName()));
-                    buttonHandlers.put(b.getId(), handler);
+                CompletableFuture.allOf(globalQueued.toArray(new CompletableFuture[0])).thenRunAsync(() -> {
+                    globalCommands.forEach(cmd -> {
+                        if (cmd != null) {
+                            cmd.delete().queue();
+                            LOGGER.info(String.format("Queued deletion of global command %s", cmd.getName()));
+                        }
+                    });
+                });
+                CompletableFuture.allOf(guildQueued.toArray(new CompletableFuture[0])).thenRunAsync(() -> {
+                    guildCommands.forEach(
+                        (key, val) -> val.forEach(cmd -> {
+                            if (cmd != null) {
+                                cmd.delete().queue();
+                                LOGGER.info(String.format("Queued deletion of command %s for guild %d", cmd.getName(), key));
+                            }
+                        })
+                    );
                 });
             }
-        });
+        );
     }
 
     @Override
